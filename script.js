b428d5085e89f6ceaeb66cf51bf877ec5e67b8dc
sergeytura <sergei.pashkov@list.ru>
2023-01-27 20:27:03 +0200

    js start

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+const search = document.querySelector('.app__search')
+search.textContent = 'ITS SEARCH'
+
+const result = document.querySelector('.app__result')
+result.textContent = 'ITS B'
+
+const input = document.querySelector('.app__input')
+
+input.addEventListener('input', (e) => {
+    fetch(`https://api.github.com/search/repositories?q=Q`)
+    .then(response => console.log(response))
+})
